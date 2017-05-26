f4961071f0348510bbe560d7f931dd7963e48963
Bawler <Oliver.Bawler@landregistry.gov.uk>
2017-05-26 09:35:54 +0100

    reworked for modularisation and new features

--- app/forms.py
+++ app/forms.py
@@ -3,25 +3,7 @@ from wtforms import StringField, FieldList, FormField, TextField
 from wtforms.validators import DataRequired
 
 class RuleForm(Form):
-    #addresses = FieldList(FormField(RuleEntryForm), min_entries=5)
     source = StringField('source', validators=[DataRequired()])
     destination = StringField('destination', validators=[DataRequired()])
     port = StringField('port', validators=[DataRequired()])
-    source1 = StringField('source1')
-    destination1 = StringField('destination1')
-    port1 = StringField('port1')
-    source2 = StringField('source2')
-    destination2 = StringField('destination2')
-    port2 = StringField('port2')
-    source3 = StringField('source3')
-    destination3 = StringField('destination3')
-    port3 = StringField('port3')
-    source4 = StringField('source4')
-    destination4 = StringField('destination4')
-    port4 = StringField('port4')
-    source5 = StringField('source5')
-    destination5 = StringField('destination5')
-    port5 = StringField('port5')
-    source6 = StringField('source6')
-    destination6 = StringField('destination6')
-    port6 = StringField('port6')
+
--- app/views.py
+++ app/views.py
@@ -15,69 +15,35 @@ def index():
 @app.route('/rules')
 def rules():
 	form = RuleForm()
-	source = request.args.get('source')
-	destination = request.args.get('destination')
-	port = request.args.get('port')
-	source1 = request.args.get('source1')
-	destination1 = request.args.get('destination1')
-	port1 = request.args.get('port1')
-	source2 = request.args.get('source2')
-	destination2 = request.args.get('destination2')
-	port2 = request.args.get('port2')
-	source3 = request.args.get('source3')
-	destination3 = request.args.get('destination3')
-	port3 = request.args.get('port3')
-	source4 = request.args.get('source4')
-	destination4 = request.args.get('destination4')
-	port4 = request.args.get('port4')
-	source5 = request.args.get('source5')
-	destination5 = request.args.get('destination5')
-	port5 = request.args.get('port5')
-	source6 = request.args.get('source6')
-	destination6 = request.args.get('destination6')
-	port6 = request.args.get('port6')
-	sourced = [source,source1,source2,source3,source4,source5,source6]
-	destd = [destination,destination1,destination2,destination3,destination4,destination5,destination6]
-	portd = [port,port1,port2,port3,port4,port5,port6]
-	sources =[]
-	dests=[]
-	ports=[]
-	for source in sourced:
-		if source != None:
-			count = len(source)
-			while count < 15:
-				source = source + " "
-				count += 1
-			sources.append(source)
-		else:
-			source = ""
-			sources.append(source)
-	for dest in destd:
-		if dest != None:
-			count = len(dest)
-			while count < 15:
-				dest = dest + " "
-				count += 1
-			dests.append(dest)
-		else:
-			dest = ""
-			dests.append(dest)
-	for port in portd:
-		if port != None:
-			ports.append(port)
-		else:
-			port = ""
-			ports.append(port)
-    #file2open = destination + ".txt"
-    #f = open(file2open, "r")
-    #text =[]
-    #while 1:
-    #    line = f.readline()
-    #    if not line:break
-    #    text.append(line)
-    #f.close()
+	theSources = request.args.getlist('source')
+	theDestinations = request.args.getlist('destination')
+	thePorts = request.args.getlist('port')
+	sources = addSpaces(theSources)
+	dests = addSpaces(theDestinations)
+	ports = addSpaces(thePorts)
+	output = formatValues(sources,dests,ports)
+		
 	return render_template('rules.html',
-							form=form,
-							sources=sources,
-							dests=dests,
-							ports=ports)
+                            form=form,
+							output=output)
+							
+def addSpaces(array):
+	result=[]
+	for a in array:
+		if a != None:
+			count = len(a)
+			while count < 17:
+				a += " "
+				count += 1
+			result.append(a)
+		else:
+			a = ""
+			result.append(a)
+	return result
+	
+def formatValues(mySources, myDests, myPorts):
+	result =[]
+	for i,source in enumerate(mySources):
+		line = mySources[i] + myDests[i] + myPorts[i]
+		result.append(line)
+	return result
